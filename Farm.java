b9640f5ba8df95e89f4fca13beef35c48c4e2877
danham1 <danham1@s.sfusd.edu>
2018-10-22 10:26:01 -0700

    done with NamedCow

--- Farm.java
+++ Farm.java
@@ -13,5 +13,6 @@ class Farm
 		{
 			System.out.println(aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
 		}
+		System.out.println("The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );
 	}
 }
